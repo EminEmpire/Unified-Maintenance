1d69bead957f57b55fb86f0db19740dc2a1a3dcd
Ahmed Emin <126208272+AEmin96@users.noreply.github.com>
2024-03-29 21:24:56 +0000

    Fix collapsed logo

--- assets/js/mainjs.js
+++ assets/js/mainjs.js
@@ -15,12 +15,12 @@ $(document).ready(function() {
     // Listen for the 'show.bs.collapse' event
     $('#navbarNavDropdown').on('show.bs.collapse', function () {
         // Navbar is about to be shown, change the logo
-        $(".header-top .navbar-brand img").attr("src", "assets/media/img/collapsedlogo.png");
+        $(".header-top .navbar-brand img").attr("src", "assets/media/img/collapsedlogo.webp");
     });
 
     // Listen for the 'hide.bs.collapse' event
     $('#navbarNavDropdown').on('hide.bs.collapse', function () {
         // Navbar is about to be hidden, revert back to the original logo
-        $(".header-top .navbar-brand img").attr("src", "assets/media/img/logo.png");
+        $(".header-top .navbar-brand img").attr("src", "assets/media/img/logo.webp");
     });
 });
