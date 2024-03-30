5c38d296ac96ac81216adcade90c4db60922881f
Ahmed Emin <126208272+AEmin96@users.noreply.github.com>
2024-03-30 04:57:21 +0000

    Change logo and styles

--- assets/js/mainjs.js
+++ assets/js/mainjs.js
@@ -10,17 +10,3 @@ $(document).ready(function() {
         window.location.href = 'contactus.html';
     });
 });
-
-$(document).ready(function() {
-    // Listen for the 'show.bs.collapse' event
-    $('#navbarNavDropdown').on('show.bs.collapse', function () {
-        // Navbar is about to be shown, change the logo
-        $(".header-top .navbar-brand img").attr("src", "assets/media/img/collapsedlogo.webp");
-    });
-
-    // Listen for the 'hide.bs.collapse' event
-    $('#navbarNavDropdown').on('hide.bs.collapse', function () {
-        // Navbar is about to be hidden, revert back to the original logo
-        $(".header-top .navbar-brand img").attr("src", "assets/media/img/logo.webp");
-    });
-});
